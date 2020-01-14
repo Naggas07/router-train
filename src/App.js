d344b94b492af8aea077fbc960833690f68b72fc
roberto araya gil <naggas@Naggas.local>
2020-01-14 21:12:03 +0100

    simple nav with react dom

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
+import Nabvar from './components/Navbar'
+import Home from './components/Home'
+import About from './components/About'
+import Where from './components/Where'
+import Catalog from './components/Catalog'
+import { Switch, Route, Redirect} from 'react-router-dom'
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nabvar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/About">
+          <About />
+        </Route>
+        <Route exact path="/Where" component={Where} />
+        <Route exact path="/Catalog">
+          <Catalog />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
     </div>
   );
 }
---
+++ src/components/Where.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Where = () => {
+    return ( 
+        <h1>Where</h1>
+     );
+}
+ 
+export default Where;
